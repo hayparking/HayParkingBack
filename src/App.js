2a0425fcadf41c9ce7a1f155a29129300b678ec0
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-09-10 14:48:11 -0500

    index, pagina principal

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">HayParking</h1>
+
+      <div>
+      <button>Inicia Seción</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
